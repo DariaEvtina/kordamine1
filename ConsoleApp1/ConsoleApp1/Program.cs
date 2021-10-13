8b410ba573aa842d4e42ba2b8ce295d0b4d0243f
DariaEvtina <77333298+DariaEvtina@users.noreply.github.com>
2021-10-13 10:53:05 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kordamine1
+{
+    class Program
+    {
+        static void Main23(string[] args)
+        {
+
+            //Ex2
+            int mult = 1;
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("enter num:");
+                int smt = Convert.ToInt32(Console.ReadLine());
+                sum += smt;
+                mult = smt * mult;
+            }
+            double avg = sum / 5;
+            Console.WriteLine($"AVG={avg}\nSUM={sum}\nMULTIPLIED={mult}");
+            //ex3
+            string nr;
+            do
+            {
+                Console.WriteLine("Tere,Osta elevant ära!");
+                nr = Console.ReadLine();
+            } while (nr != "elevant");
+            Console.WriteLine("elevant!");
+
+            //ex4
+            Random randNum = new Random();
+            int A = randNum.Next(1, 15);
+            int AA;
+            int K = 1;
+            Console.WriteLine("arvake arv 1 kuni 15:");
+            do
+            {
+                Console.WriteLine("{0} Katse. Mis on arv?", K);
+                AA = int.Parse(Console.ReadLine());
+                K++;
+            } while (AA != A && K != 6);
+            if (AA == A)
+            {
+                Console.WriteLine("Palju õnne");
+            }
+            else
+            {
+                Console.WriteLine("Proovi uuesti");
+            }
+            //ex5
+            int ridadearv = 10, veergudearv = 10;
+            if (args.Length == 2)
+            {
+                ridadearv = int.Parse(args[0]);
+                veergudearv = int.Parse(args[1]);
+            }
+            for (int rida = 0; rida < ridadearv; rida++)
+            {
+                for (int veerg = 0; veerg < veergudearv; veerg++)
+                {
+                    Console.Write("{0, 5}", rida * veerg);
+                }
+                Console.WriteLine();
+            }
+            //ex6
+            int arv;
+            int[] arvud = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Sisetaja {0} arv!", i + 1);
+                arv = int.Parse(Console.ReadLine());
+                arvud[i] = arv;
+            }
+            int arv4 = 0;
+            Array.Sort(arvud);
+            Array.Reverse(arvud);
+            foreach (var a in arvud)
+            {
+                arv4 = arv4 * 10 + a;
+            }
+            Console.Write(arv4);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/avanProgramm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class avanProgramm
+    {
+        static int calculator(int a,int b, string c)
+        {
+            if (c=="+")
+            {
+                return a + b;
+            }
+            else if (c=="/")
+            {
+                return a / b;
+            }
+            else if (c=="-")
+            {
+                return a - b;
+            }
+            else if (c=="*")
+            {
+                return a * b;
+            }
+            return 0;
+        }
+        public static int keskmine(int a, int c)
+        {
+            return a / c;
+        }
+        public static int korruta(int arv1, int arv2)
+        {
+            return arv1 * arv2;
+        }
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 23;
+            Console.WriteLine("{0} * {1} = {2}",a,b,korruta(a, b));
+            //ex1
+            Console.WriteLine("sisestage mitu numbrit soovite keskmist leida?");
+            int c = int.Parse(Console.ReadLine());
+            int[] arvud = new int[c];
+            for (int i = 0; i < c; i++)
+            {
+                Console.WriteLine("Sisetaja {0} num:", i);
+                int arvu = int.Parse(Console.ReadLine());
+                arvud[i] = arvu;
+            }
+            int arv=0;
+            foreach (var avr in arvud)
+            {
+                arv += avr;
+            }
+            Console.WriteLine("Keskmine arv: {0}",keskmine(arv,c));
+            //ex2
+            Console.WriteLine("sisetaja esimene arv:");
+            int nr1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("sisetaja teine arv:");
+            int nr2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("sisetaja operatsioon(+,/,*,-):");
+            string op = Console.ReadLine();
+            int Result = calculator(nr1, nr2, op);
+            Console.WriteLine("{0} {1} {2}={3}",nr1,op,nr2,Result);
+            for (int i = 0; i < Result; i++)
+            {
+                Console.WriteLine("nur");
+            }
+            Console.ReadLine();
+            
+
+        }
+        
+    }
+}
